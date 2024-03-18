21c279fc8191302b2e393d4cf1a433187ad28b2f
luizgfalqueto <lgfalqueto85@gmail.com>
2024-03-18 14:55:18 -0300

    fix: fixing import material

--- lib/src/modules/home/home_module.dart
+++ lib/src/modules/home/home_module.dart
@@ -1,5 +1,5 @@
 import 'package:fe_lab_clinicas_self_service/src/modules/home/home_page.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 
 class HomeModule extends FlutterGetItModule {
