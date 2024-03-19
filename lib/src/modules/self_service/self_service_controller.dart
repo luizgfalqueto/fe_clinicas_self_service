2b2e801be929cd6b451495e4fee2d4cadd77ddcb
luizgfalqueto <lgfalqueto85@gmail.com>
2024-03-19 11:38:03 -0300

    feat: documents page navigating to scan page

--- lib/src/modules/self_service/self_service_controller.dart
+++ lib/src/modules/self_service/self_service_controller.dart
@@ -16,7 +16,9 @@ enum FormSteps {
 
 class SelfServiceController with MessageStateMixin {
   final _step = ValueSignal(FormSteps.none);
+
   var _model = const SelfServiceModel();
+
   SelfServiceModel get model => _model;
 
   FormSteps get step => _step();
@@ -50,5 +52,24 @@ class SelfServiceController with MessageStateMixin {
   void updatePatientAndGoDocument(PatientModel? patient) {
     _model = _model.copyWith(patient: () => patient);
     _step.forceUpdate(FormSteps.documents);
+  }
+
+  void registerDocument(DocumentType type, String filePath) {
+    final documents = _model.documents ?? {};
+
+    if (type == DocumentType.healthInsuranceCard) {
+      documents[type]?.clear();
+    }
+
+    final values = documents[type] ?? [];
+    values.add(filePath);
+    documents[type] = values;
+    _model = _model.copyWith(
+      documents: () => documents,
+    );
+  }
+
+  void clearDocuments(){
+    _model = _model.copyWith(documents: () => {},);
   }
 }
